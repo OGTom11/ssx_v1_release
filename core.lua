e75ef6d469beb5bd07d19010ce49ff4fc54f545b
Thomas Barrett <tombarrettyt@gmail.com>
2023-03-01 18:33:16 +0000

    Initial release of SSX. Expect bugs & issues.

---
+++ core.lua
@@ -0,0 +1,44 @@
+local net = Instance.new("BindableEvent")
+net.Name = "Network"
+net.Parent = script.Parent
+
+
+playersInGame = {}
+local DSS = game:GetService("DataStoreService")
+playersBanned = DSS:GetDataStore("playersBanned")
+discordW = false
+
+
+game.Players.PlayerAdded:Connect(function(plr)
+	print(plr.Name.." joined "..game.Name)
+	
+	playersBanned:RemoveAsync(plr.Name, plr.LocaleId)
+	
+	if playersBanned:GetAsync(plr.Name, plr.LocaleId) then -- Ban Checker
+		plr:Kick("ssx Anti Exploit | "..plr.Name.." is banned.")
+	else
+		return
+	end
+end)
+
+
+net.Event:Connect(function(player, offense)
+	if offense == "walkspeed" then
+		player:Kick("ssx Anti Exploit | Player Speed mismatch.")
+		playersBanned:SetAsync(player.Name, player.LocaleId)
+	end
+
+	
+end)
+
+game.Players.PlayerAdded:Connect(function(plr)
+	plr.CharacterAdded:Connect(function(character)
+		local Humanoid = plr.Character.Humanoid
+
+		Humanoid.Running:Connect(function(speed)
+			if speed > 16 then
+				net:Fire(plr, "walkspeed")
+			end
+		end)
+	end)
+end)
